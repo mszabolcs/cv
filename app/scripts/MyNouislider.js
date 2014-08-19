bc7095b4aba1b933d4f6a6af72f2860c22993476
Molnár Szabolcs <molnar.szabolcs.privat@gmail.com>
2014-08-19 16:33:18 +0200

    huge upgrades

---
+++ app/scripts/MyNouislider.js
@@ -0,0 +1,66 @@
+
+// Otherwise, the HTML will be inserted into the handle.
+// One level of HTML is supported.
+var customToolTip = $.Link({
+	target: '-tooltip-<div class="ui-tooltip"></div>',
+	method: function ( value ) {
+
+		// The tooltip HTML is 'this', so additional
+		// markup can be inserted here.
+		$(this).html(
+			'<strong>Év: </strong>' +
+			'<span>' + parseInt(value) + '</span>'
+		);
+	}
+});
+
+$("#range-slider").noUiSlider({
+		start: [1998,2014],
+		connect: true,
+		step: 1,
+		behaviour: 'drag',
+		range: {
+			'min': 1998,
+			'max': 2014
+		},
+		serialization: {
+			lower: [ customToolTip ],
+			upper: [ customToolTip ]
+		}
+});
+
+$("#range-slider").change(function(){
+	changeSchools();
+});
+
+function changeSchools(){
+	var start_end = $("#range-slider").val();
+	if(start_end[0]<=2006){
+		if($(".kossuth-thumb").hasClass("bounceOutDown")){$(".kossuth-thumb").removeClass("bounceOutDown");}
+		if(!$(".kossuth-thumb").hasClass("bounceInDown")){$(".kossuth-thumb").addClass("bounceInDown");}
+	}
+	else
+	{
+		if($(".kossuth-thumb").hasClass("bounceInDown")){$(".kossuth-thumb").removeClass("bounceInDown");};
+		if(!$(".kossuth-thumb").hasClass("bounceOutDown")){$(".kossuth-thumb").addClass("bounceOutDown");}
+	}
+	if(start_end[1]>=2010){
+		if($(".corvinus-thumb").hasClass("bounceOutUp")){$(".corvinus-thumb").removeClass("bounceOutUp");}
+		if(!$(".corvinus-thumb").hasClass("bounceInUp")){$(".corvinus-thumb").addClass("bounceInUp");}
+	}
+	else
+	{
+		if($(".corvinus-thumb").hasClass("bounceInUp")){$(".corvinus-thumb").removeClass("bounceInUp");};
+		if(!$(".corvinus-thumb").hasClass("bounceOutUp")){$(".corvinus-thumb").addClass("bounceOutUp");}
+	}
+	if((start_end[0]>=1998 && start_end[0]<=2010) && (start_end[1]<=2014 && start_end[1]>=2006)){
+		if($(".foldes-thumb").hasClass("bounceOut")){$(".foldes-thumb").removeClass("bounceOut");}
+		if(!$(".foldes-thumb").hasClass("bounceIn")){$(".foldes-thumb").addClass("bounceIn");}
+	}
+	else
+	{
+		if($(".foldes-thumb").hasClass("bounceIn")){$(".foldes-thumb").removeClass("bounceIn");};
+		if(!$(".foldes-thumb").hasClass("bounceOut")){$(".foldes-thumb").addClass("bounceOut");}
+	}
+
+}
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,21 @@
 console.log('\'Allo \'Allo!');
+
+$(document).ready(function (){
+
+});
+/*
 function openSection(element){
 	var selected = "."+element+""
 	if($(selected).hasClass("glyphicon-chevron-down")){
 		$(selected).removeClass("glyphicon-chevron-down");
 		$(selected).addClass("glyphicon-chevron-up");
+		//$(selected).addClass("glyphicon-chevron-up").trigger("classChange");		
+		
+		$("#work-zms").addClass("bounceInLeft");
+		$("#work-hdagency").addClass("bounceInLeft");
+	
+		$("#work-zms").css("display","block");
+		$("#work-hdagency").css("display","block");
 	}
 	else
 	{
@@ -11,3 +23,16 @@ function openSection(element){
 		$(selected).addClass("glyphicon-chevron-down");
 	}
 }
+*/
+function animateWork(){
+
+}
+
+/*
+$(".szakma").on("classChange", function(){
+	$("#work-zms").addClass("wobble");
+	$("#work-hdagency").addClass("wobble");
+
+	$("#work-zms").css("display","block");
+	$("#work-hdagency").css("display","block");
+});*/
