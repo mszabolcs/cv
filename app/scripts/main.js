0f877b8bdfc65d6c8444e720af86137dbf1648dd
Molnár Szabolcs <molnar.szabolcs.privat@gmail.com>
2014-08-21 15:24:47 +0200

    nyelvtudas updated and working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,19 +3,12 @@ console.log('\'Allo \'Allo!');
 $(document).ready(function (){
 
 });
-/*
+
 function openSection(element){
 	var selected = "."+element+""
 	if($(selected).hasClass("glyphicon-chevron-down")){
 		$(selected).removeClass("glyphicon-chevron-down");
 		$(selected).addClass("glyphicon-chevron-up");
-		//$(selected).addClass("glyphicon-chevron-up").trigger("classChange");		
-		
-		$("#work-zms").addClass("bounceInLeft");
-		$("#work-hdagency").addClass("bounceInLeft");
-	
-		$("#work-zms").css("display","block");
-		$("#work-hdagency").css("display","block");
 	}
 	else
 	{
@@ -23,7 +16,7 @@ function openSection(element){
 		$(selected).addClass("glyphicon-chevron-down");
 	}
 }
-*/
+
 function animateWork(){
 
 }
